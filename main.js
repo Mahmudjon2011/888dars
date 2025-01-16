65df5c2b27229fcce6811a262bcfdca1f57d5c71
Mahmudjon2011 <dildooraxajayeva@gmail.com>
2025-01-16 21:57:14 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+// const oquvchilar = {
+//     ismi:"Mahmud",
+//     yosh:13,
+//     uylanganmi:false,
+// }
+// console.log(oquvchilar);
+const userName = prompt("Ismingizni kiriting")
+const id = +prompt("Spase idingizni kiriting")
+const password = prompt("Spase passwordingizni kiriting")
+
+
+const oquvchilar = {
+    name:userName,
+    id: id,
+    password:password,
+}
+console.log(oquvchilar);
